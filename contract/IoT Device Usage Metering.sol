51c0cc8b816318c75dc39f011da76bb101a11389
SachitaTiwari <153295289+SachitaTiwari@users.noreply.github.com>
2025-04-26 17:48:16 +0530

    Update IoT Device Usage Metering.sol

--- contract/IoT Device Usage Metering.sol
+++ contract/IoT Device Usage Metering.sol
@@ -1,23 +1,44 @@
-mapping(address => bool) private _registeredDevices;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-event DeviceRegistered(address indexed device);
-event DeviceDeregistered(address indexed device);
+contract DeviceRegistry {
+    mapping(address => bool) private _registeredDevices;
 
-function registerDevice(address device) external onlyOwner {
-    if (device == address(0)) revert InvalidInput();
-    _registeredDevices[device] = true;
-    emit DeviceRegistered(device);
-}
+    event DeviceRegistered(address indexed device);
+    event DeviceDeregistered(address indexed device);
 
-function deregisterDevice(address device) external onlyOwner {
-    if (!_registeredDevices[device]) revert InvalidInput();
-    _registeredDevices[device] = false;
-    emit DeviceDeregistered(device);
-}
+    address public owner;
 
-modifier onlyRegistered(address device) {
-    if (!_registeredDevices[device]) revert InvalidInput();
-    _;
-}
+    error InvalidInput();
+    error NotOwner();
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
+
+    modifier onlyRegistered(address device) {
+        if (!_registeredDevices[device]) revert InvalidInput();
+        _;
+    }
+
+    function registerDevice(address device) external onlyOwner {
+        if (device == address(0)) revert InvalidInput();
+        _registeredDevices[device] = true;
+        emit DeviceRegistered(device);
+    }
+
+    function deregisterDevice(address device) external onlyOwner {
+        if (!_registeredDevices[device]) revert InvalidInput();
+        _registeredDevices[device] = false;
+        emit DeviceDeregistered(device);
+    }
+
+    // 🆕
+
 
 
